8bfe25dbae0a219fd73be63defa178faeb91beb8
Wout Schellaert <wout.schellaert@gmail.com>
2021-11-17 17:53:39 +0100

    Rename record types file

--- assessors/core/__init__.py
+++ assessors/core/__init__.py
@@ -2,4 +2,4 @@ from assessors.core.model import ModelDefinition, TrainedModel, Restore
 from assessors.core.model_tensorflow import TFModelDefinition
 from assessors.core.dataset import DatasetDescription, Dataset
 from assessors.core.dataset_tensorflow import TFDatasetDescription, TFDataset, CustomDatasetDescription, CSVDatasetDescription
-from assessors.core.datatypes import PredictionRecord, AssessorPredictionRecord, TypedPredictionRecord
+from assessors.core.record import PredictionRecord, AssessorPredictionRecord, TypedPredictionRecord
